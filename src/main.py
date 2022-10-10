4300a1111b64ec7a1670b6963ab4478191d7387c
aktivgo <aktivgo01062001@gmail.com>
2022-10-10 22:43:49 +0300

    Finished lab

--- src/main.py
+++ src/main.py
@@ -4,3 +4,19 @@ if __name__ == "__main__":
     modules = list(map(int, input('input modules: ').split(' ')))
     src = SRC(modules)
 
+    a = int(input('input integer number: '))
+    b = int(input('input integer number: '))
+
+    a_src = src.from_dec_to_src(a)
+    b_src = src.from_dec_to_src(b)
+
+    print('a in src =', a_src)
+    print('b in src =', b_src)
+
+    sum = src.sum(a_src, b_src)
+    diff = src.diff(a_src, b_src)
+    mult = src.mult(a_src, b_src)
+
+    print('a(', a_src, ') + b(', b_src, ') =', sum, '(', str(src.from_src_to_dec(sum)), ')')
+    print('a(', a_src, ') - b(', b_src, ') =', diff, '(', str(src.from_src_to_dec(diff)), ')')
+    print('a(', a_src, ') * b(', b_src, ') =', mult, '(', str(src.from_src_to_dec(mult)), ')')
--- src/src.py
+++ src/src.py
@@ -1,31 +1,46 @@
+import math
+
+
 class SRC:
     def __init__(self, modules: list[int]):
         self.modules = modules
+        self.M = math.prod(self.modules)
+        print('M =', self.M)
+
+    def __validate_number(self, a: [int]):
+        if len(a) != len(self.modules):
+            raise BaseException('length of Number not equal length M')
+        for i in range(len(self.modules)):
+            if a[i] >= self.modules[i]:
+                raise BaseException('Number contains number equal o greater then M[i]')
 
     def __validate_numbers(self, a: [int], b: [int]):
-        if len(a) > len(self.modules):
-            raise BaseException('length A is greater then length M')
-        if len(b) > len(self.modules):
-            raise BaseException('length B is greater then length M')
-
-        for i in range(0, len(self.modules)):
-            if a[i] >= self.modules[i]:
-                raise BaseException('A contains number equal o greater then M[i]')
-            if b[i] >= self.modules[i]:
-                raise BaseException('B contains number equal o greater then M[i]')
+        self.__validate_number(a)
+        self.__validate_number(b)
 
     def from_dec_to_src(self, num) -> [int]:
         return [num % m for m in self.modules]
 
-    def from_src_to_dec(self, ) -> int:
-        # TODO: implement
-        return 0
+    def from_src_to_dec(self, a: [int]) -> int:
+        self.__validate_number(a)
+
+        ci = []
+        for i in range(len(a)):
+            mi = int(self.M / self.modules[i])
+            m_inv = mi % self.modules[i]
+            ci.append(mi * m_inv)
+
+        x0 = 0
+        for i in range(len(a)):
+            x0 += a[i] * ci[i]
+
+        return x0 % self.M
 
     def sum(self, a: [int], b: [int]) -> [int]:
         self.__validate_numbers(a, b)
 
         result = []
-        for i in (0, len(self.modules) - 1):
+        for i in range(len(self.modules)):
             result.append((a[i] + b[i]) % self.modules[i])
 
         return result
@@ -34,7 +49,7 @@ class SRC:
         self.__validate_numbers(a, b)
 
         result = []
-        for i in (0, len(self.modules) - 1):
+        for i in range(len(self.modules)):
             result.append((a[i] - b[i]) % self.modules[i])
 
         return result
@@ -43,7 +58,7 @@ class SRC:
         self.__validate_numbers(a, b)
 
         result = []
-        for i in (0, len(self.modules) - 1):
+        for i in range(len(self.modules)):
             result.append((a[i] * b[i]) % self.modules[i])
 
         return result
